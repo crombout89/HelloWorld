32847e33146c7553db2affa27e24d34d70934a63
Caley Rombout <crombout@my.bcit.ca>
2025-05-14 10:59:00 -0700

    Modularize notifications logic

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,11 +1,7 @@
-  const socket = io();
+const socket = io();
 
-  // For now, join room using current user ID from server
-  <% if (user && user._id) { %>
-    socket.emit('join', '<%= user._id %>');
-  <% } %>
+if (window.userId) {
+  socket.emit("join", window.userId);
+}
 
-  socket.on('notification', data => {
-    alert('🔔 New Notification: ' + data.message);
-    // You could also update the DOM instead of using alert
-  });
+window.appSocket = socket; // make it accessible globally
