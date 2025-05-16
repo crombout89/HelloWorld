17bbc02a442b0ae379e8a5c07a52607f417dce05
faghaarabi <faghaarabi@my.bcit.ca>
2025-05-15 23:17:43 -0700

    Test Functionality

--- routes/inviteRoute.js
+++ routes/inviteRoute.js
@@ -14,7 +14,7 @@ router.post('/invite', async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: 'gmail', // You can change this to your email provider
+      service: 'gmail',
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
